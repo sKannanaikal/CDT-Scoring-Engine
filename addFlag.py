dadeb54fa6801241be7603004320b386a1e25239
Sean Kannanaikal <stk5106@g.rit.edu>
2023-10-21 17:52:37 -0400

    Update addFlag.py

--- addFlag.py
+++ addFlag.py
@@ -3,7 +3,7 @@ import requests
 import socket
 
 def main():
-    prefix = ['rt', 'redteam', 'rteam', 'red', 'russia']
+    prefix = ['rt', 'redteam', 'rteam', 'red', 'russia', 'soviets', 'motherRussia', 'motherrussia', 'russians']
     hostname = socket.gethostname()
     filename = input('Enter the full file path to save/name file: ')
     message = input ('Enter the message you would like to embed in flag: ')
@@ -13,12 +13,12 @@ def main():
         flagFile.write(flag)
 
     
-    scoringServer = f'http://127.0.0.1:5000/{hostname}' 
+    scoringServer = f'http://192.168.100.10:80/{hostname}' 
 
     data  = {'flag': f'{flag}','location': f'{filename}','team': 'red', 'hostname': {hostname}}
 
     request = requests.post(scoringServer, data=data)
     print(request.text)
 
-    #make a post request to scoring server
-main()
+if __name__ == '__main__':
+    main()
